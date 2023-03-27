cdcb9b8df01dd4e297fd5c1240715c1b2fe458ae
Vincent R <renard.vincent@protonmail.com>
2023-03-27 13:26:10 +0000

    dbt model done

---
+++ dbt/dbtcloud/models/polit-models/threads.sql
@@ -0,0 +1,12 @@
+{{ config(materialized="table") }}
+
+with
+    old_threads as (select * from `pol-it-de-zoomcamp.dbt_vrenard.threads`),
+    new_threads as (select * from `pol-it-de-zoomcamp.pol_data.threads_raw`)
+select *
+from new_threads
+union all
+select *
+from old_threads
+where old_threads.thread_id not in (select thread_id from new_threads)
+order by thread_id
