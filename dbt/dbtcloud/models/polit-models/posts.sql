ec3b1d130956035b04c5f6bf04499407a766933a
Vincent R <renard.vincent@protonmail.com>
2023-03-28 00:37:28 +0000

    Change to incremental model

--- dbt/dbtcloud/models/polit-models/posts.sql
+++ dbt/dbtcloud/models/polit-models/posts.sql
@@ -1,18 +1,14 @@
-{{ config(
-    materialized='incremental',
-    partition_by={
-      "field": "post_datetime",
-      "data_type": "timestamp",
-      "granularity": "hour"
-    }
-)}}
+{{
+    config(
+        materialized="incremental",
+        unique_key="post_id",
+        partition_by={
+            "field": "post_datetime",
+            "data_type": "timestamp",
+            "granularity": "hour",
+        },
+    )
+}}
 
-with
-    old_posts as (select * from `pol-it-de-zoomcamp.dbt_vrenard.posts`),
-    new_posts as (select * from `pol-it-de-zoomcamp.pol_data.posts_raw`)
 select *
-from new_posts
-union all
-select *
-from old_posts
-where old_posts.post_id not in (select post_id from new_posts)
+from `pol-it-de-zoomcamp.pol_data.posts_raw`
